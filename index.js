6efc3b0e9d960fb2519aec38445d7352f24b1084
Sam Robinson <110217039+SamRobinson-2511@users.noreply.github.com>
2022-11-06 16:45:09 -0600

    commit tree 4cbbf334a1fbd4cf2da311283c91d5bd99833df8

--- index.js
+++ index.js
@@ -1,1 +1,74 @@
 // Add your code here
+
+// const formData = {
+//     dogName: "Byron",
+//     dogBreed: "Poodle"
+// }
+
+
+// const configurationObject = {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json"
+//     },
+//     body: JSON.stringify(formData),
+// }
+// const dogs = "http://localhost:3000/dogs"
+// fetch(dogs, configurationObject)
+//     .then(response => response.json())
+//     .then(object => console.log(object))
+//     // .then(function(response){
+//     //     return response.json();
+//     // })
+//     // .then(function(object){
+//     //     console.log(object);
+//     // })
+
+// const configurationObject = {
+//     headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json"
+//     },
+//     body: JSON.stringify(formData),
+// };
+
+// fetch(dogs, configurationObject)
+//     .then(res => res.json())
+//     .then(dog => console.log(dog))
+//     .catch(error => {
+//         alert('Bad Things')
+//         console.log(error.message)
+//     }
+// )
+
+
+// const configurationObject = {
+//     method: "POST",
+//     headers: {
+//         "Content-Type": "application/json",
+//         "Accept": "application/json"
+//     },
+//     body: JSON.stringify(formData),
+// }
+const formObj = {
+    userName: "sam",
+    userEmail: "xxx@xxxx.com"
+}
+
+const config = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify(formObj)
+}
+const users = "http://localhost:3000/users"
+
+    fetch(users, formObj)
+        .then(response => response.json())
+        .then(dat => {
+            console.log(user)
+        })
+
